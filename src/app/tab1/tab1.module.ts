f38e78ccec04b63acce98e1df4459e8b38bc8b17
Łukasz Langer <lukaszzdw1998@gmail.com>
2022-03-21 16:12:08 +0100

    added basic layout and working barcode scanner feature

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  providers: [BarcodeScanner]
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,58 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner, public alertController: AlertController) {}
+
+  public listNumber: number = 0;
+  public itemString: string = '';
+  items: string[];
+
+  async scan()
+  {
+    this.barcodeScanner.scan().then(async (barcodeData) => {
+      /*const alert = await this.alertController.create({
+        header: 'Kod kreskowy',
+        message: barcodeData.text,
+      });
+      await alert.present();*/
+      if(barcodeData.text != '')
+      {
+        this.listNumber++;
+        this.addItemToList(barcodeData.text);
+      }
+    }, async (err)=>{
+      const alert = await this.alertController.create({
+        header: 'Wystąpił błąd',
+        message: JSON.stringify(err),
+      });
+      await alert.present();
+    })
+  }
+
+  addItemToList(item: string)
+  {
+
+    this.itemString += `
+      <ion-item>
+        <ion-avatar item-start>
+          <img width="50" height="50" src="assets\\img\\item.png">
+        </ion-avatar>
+        <ion-label>${item}</ion-label>
+        <ion-note item-end>3:43 pm</ion-note>
+      </ion-item>
+      <br>
+    `;
+
+    return this.itemString;
+  }
+
+   generateList() {
+    return `
+    <ion-list>
+      ${this.itemString}
+    </ion-list>
+  `;
+  }
+
 
 }
